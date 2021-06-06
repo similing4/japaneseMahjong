cfac723ed66f2e0c2143b08a45c99e05833afd51
圣心语 <similing4@163.com>
2021-06-06 14:23:02 +0800

    修复连风牌计算问题

--- src/core/yaku/pu_tong_yi/YiPai.js
+++ src/core/yaku/pu_tong_yi/YiPai.js
@@ -25,8 +25,11 @@ export default class YiPai extends Yi {
 		var mianziList = hePaiPaixing.getAllMianzi();
 		var ret = [];
 		mianziList.map((mianzi)=>{
-			if(mianzi.isKezi() && mianzi.basePai.isYiPai(state))
+			if(mianzi.isKezi() && mianzi.basePai.isYiPai(state)){
+				if(mianzi.basePai.isLianFengPai(state))
+					ret.push(new YiPai(mianzi.basePai.getNatureName()));
 				ret.push(new YiPai(mianzi.basePai.getNatureName()));
+			}
 		});
 		return ret;
 	}
