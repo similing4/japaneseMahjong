548ec147c43a1faeccec9fca4ae0b74250cf81fe
李业宏扬 <845206213@qq.com>
2022-02-26 10:54:56 +0800

    修复平和荣和符数错误

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ import JapaneseMaj from "./src/JapaneseMaj.js";
 var maj = new JapaneseMaj({
 	changFeng: 2, // Number类型，东风场为1，南风场为2，西风场为3，北风场为4
 	ziFeng: 2, // Number类型，自风，东1南2西3北4
-	isZimo: true,
+	isZimo: false,
 	dora: [],
 	beidora: 0
 });
-var paixing = JapaneseMaj.getPaixingFromString("11221m 123m 345m 456m");
+var paixing = JapaneseMaj.getPaixingFromString("123456m12344s567p");
 var res = maj.getYakuCalculator(paixing);
 if(res)
 	console.log(res.calcYaku(maj.state));
